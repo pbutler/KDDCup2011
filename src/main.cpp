ef2fa160199a3ea6ac8b2d6eeee3724f732d31eb
Patrick Butler <pbutler@killertux.org>
2011-05-03 02:17:45 -0400

    more working

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <cmath>
-#include<stdio.h>
-#include<stdlib.h>
+#include <cstdio>
+#include <cstdlib>
 #include <ctype.h>
 #include <unistd.h>
 #include <sys/stat.h>
@@ -14,48 +14,43 @@
 #include "barrier.h"
 #include <sys/time.h>
 
+using namespace std;
 
-double itemStep				= 0.005;
-double itemReg					= 1;
-double userStep				= 1.5;
-double userReg					= 1;
+double itemStep		= 0.005;
+double itemReg		= 1;
+double userStep		= 1.5;
+double userReg		= 1;
 
 double GAMMA = .005; //.07;
-double GAMMA2 = .001; // / 10000;//.0001;
-double LAMBDA = 0.05;
+double GAMMA2 = .001/ 1; // / 10000;//.0001;
+double LAMBDA = 0.005;
 double LAMBDA2 =  GAMMA2*40;
 #define NUM_THREADS 1
 using namespace __gnu_cxx;
 using namespace std;
 
-#define SCORENORM  100
+#define SCORENORM  100.f
 
-int maxepochs = 10;
-#define TEST 1
-#undef TEST
-#ifndef TEST
-unsigned int nUsers = 1000990;
-unsigned int nItems = 624961;
-unsigned int nRatings = 252800275;
-unsigned int nValidations = nUsers*4;
-#define VFILE  "../track1/validationIdx1.txt"
-#define TFILE  "../track1/trainIdx1.txt"
-#else
-unsigned int nUsers = 44;
-unsigned int nItems = 624961;
-unsigned int nRatings = 11696;
-unsigned int nValidations = nUsers*4;
-#define VFILE  "../testdata/validationIdx1.txt"
-#define TFILE  "../testdata/trainIdx1.txt"
-#endif
+int maxepochs = 20;
+int maxepochsbias = 20;
+int maxfaults = 2;
+
+unsigned int nUsers = 0;
+unsigned int nItems = 0;
+unsigned int nRatings = 0;
+unsigned int nValidations =0 ;
+unsigned int nTests = 0;
+#define VFILE  "validationIdx1.txt"
+#define TFILE  "trainIdx1.txt"
+#define TESTFILE  "testIdx1.txt"
+
 
 const unsigned int nFeatures = 50;
 
 struct rating_s {
-	unsigned int user : 20;
+	//unsigned int user : 20;
 	unsigned int item : 20;
-	float rating;
-	//unsigned int rating : 8;
+	unsigned int rating : 8;
 };
 
 struct item_s {
@@ -66,7 +61,8 @@ struct item_s {
 struct user_s {
 	uint32_t id;
 	uint32_t count;
-	uint32_t nu;
+	uint8_t nu;
+	uint8_t qu;
 };
 
 
@@ -80,6 +76,7 @@ struct rating_s *ratings;
 struct item_s *items;
 struct user_s *users;
 struct rating_s *validations;
+struct rating_s *tests;
 double mu;
 
 pthread_barrier_t barrier;
@@ -112,7 +109,7 @@ inline int parseInt(char *s, ssize_t *i, int *a, ssize_t size) {
 	return *i == size;
 }
 
-void *open_read(char *file, ssize_t *size)
+void *open_read(const char *file, ssize_t *size)
 {
 	struct stat sbuf;
 	int f = open(file, O_RDONLY);
@@ -132,7 +129,7 @@ void *open_read(char *file, ssize_t *size)
 	return data;
 }
 
-void *open_rw(char *file, ssize_t size)
+void *open_rw(const char *file, ssize_t size)
 {
 	struct stat sbuf;
 	int f = open(file, O_APPEND|O_RDWR|O_CREAT, 0664);
@@ -157,6 +154,7 @@ void load_model()
 	bu = (double*)open_rw("tmp/bu.mmap", sizeof(double)*nUsers);
 	ratings = (struct rating_s*)open_rw("tmp/ratings.mmap", sizeof(rating_s)*nRatings);
 	validations= (struct rating_s*)open_rw("tmp/validations.mmap", sizeof(rating_s)*nValidations);
+	tests = (struct rating_s*)open_rw("tmp/tests.mmap", sizeof(rating_s)*nTests);
 	items = (struct item_s*)open_rw("tmp/items.mmap", sizeof(item_s)*nItems);
 	users = (struct user_s*)open_rw("tmp/users.mmap", sizeof(user_s)*nUsers);
 
@@ -179,36 +177,43 @@ void *init_model(void *ptr = NULL) {
 	       bi[i] = 0;
 	}
 	double sq, err=1e6, pred, lasterr = 1e6+1, faults = 0;
-	while (faults <= 2) {
+	int epoch=0;
+	while (epoch < maxepochsbias && faults < maxfaults) {
 		double start = get_time();
 		lasterr = err;
 		sq = 0;
-		for(int r = rank; r < nRatings; r+=NUM_THREADS ) {
-			pred = mu + bu[ratings[r].user] + bi[ratings[r].item];
-			if (pred < 0) pred = 0;
-			if (pred > 100/SCORENORM) pred = 100/SCORENORM;
-			err = ratings[r].rating - pred;
-			sq += err*err*SCORENORM*SCORENORM;
-			bu[ratings[r].user] += userStep*(err - userReg*bu[ratings[r].user]);
-			bi[ratings[r].item] += itemStep*(err - itemReg*bi[ratings[r].item]);
-			//printf("%g\n", bi[ratings[r].item]);
+		int ridx = 0;
+		for(int u = 0; u < nUsers; u++) {
+			for(int r = 0; r < users[u].count; r++,ridx++) {
+				struct rating_s rating = ratings[ridx];
+				pred = mu + bu[u] + bi[rating.item];
+				if (pred < 0) pred = 0;
+				if (pred > 100./SCORENORM) pred = 100./SCORENORM;
+				err = (double)rating.rating/SCORENORM - pred;
+				sq += err*err*SCORENORM*SCORENORM;
+				bu[u] += userStep*(err - userReg*bu[u]);
+				bi[rating.item] += itemStep*(err - itemReg*bi[rating.item]);
+				//printf("%g\n", bi[ratings[r].item]);
+
+			}
 		}
 		sq = sqrt(sq / (double)nRatings);
 		double vsq = 0;
-		for(int i = 0; i < nValidations; i++) {
-			pred = mu + bu[validations[i].user];
-			if (items[validations[i].item].count > 0) {
-				pred += bi[validations[i].item];
+		ridx = 0;
+		for(int u = 0; u < nUsers; u++) {
+			for(int i = 0; i < users[u].nu; i++, ridx++) {
+				struct rating_s rating=validations[ridx];
+				pred = mu + bu[u] + bi[rating.item];
+				if (pred < 0) pred = 0;
+				if (pred > 100./SCORENORM) pred = 100./SCORENORM;
+				err = (double)rating.rating/SCORENORM - pred;
+				vsq += err*err*SCORENORM*SCORENORM;
 			}
-			if (pred < 0) pred = 0;
-			if (pred > 100/SCORENORM) pred = 100/SCORENORM;
-			err = validations[i].rating - pred;
-			vsq += err*err*SCORENORM*SCORENORM;
-			//printf("%g %g %g\n", err, validations[i].rating, pred, bu[ratings[i].user]);
+			//printf("%g %d %g\n", err, validations[i].rating, pred);
 		}
 		vsq = sqrt(vsq / (double)nValidations);
 		if (rank == 0 ) {
-			printf("BIAS RMSE=%g VRMSE=%g time=%g %g\n", sq, vsq, get_time()-start, sq);
+			printf("BIAS epoch=%d RMSE=%g VRMSE=%g time=%g\n", epoch, sq, vsq, get_time()-start, sq);
 		}
 		err = vsq;
 		if (err > lasterr) {
@@ -218,6 +223,7 @@ void *init_model(void *ptr = NULL) {
 			userStep *= .7;
 			faults = 0;
 		}
+		epoch++;
 	}
 	for(int i = rank; i < nUsers*nFeatures;i+=NUM_THREADS) {
 		p[i] =  0; //randF(-.003, .001);
@@ -235,13 +241,11 @@ double predict(int uid, int iid)
 {
 	double sum = mu;
 	sum += bu[uid];
-	if ( items[iid].count > 0) {
-		sum += bi[iid];
-		for(int f = 0; f < nFeatures; f++) {
-			sum += q[iid*nFeatures+f] * p[uid*nFeatures+f];
-		}
+	sum += bi[iid];
+	for(int f = 0; f < nFeatures; f++) {
+		sum += q[iid*nFeatures+f] * p[uid*nFeatures+f];
 	}
-	if (sum > 100/SCORENORM) sum = 100 /SCORENORM ;
+	if (sum > 100./SCORENORM) sum = 100. /SCORENORM ;
 	if (sum < 0) sum = 0;
 	//printf("* %g %g \n", mu + bu[uid] + bi[iid], sum);
 
@@ -251,17 +255,78 @@ double predict(int uid, int iid)
 double validate(bool print = false)
 {
 	double sq = 0;
-	for(int i = 0; i < nValidations; i++) {
-		double pred = predict(validations[i].user, validations[i].item);
-		double err = validations[i].rating - pred;
-		sq += err*err*SCORENORM*SCORENORM;
-		if (print) {
-			printf("%d %d %g %g %g\n", validations[i].user, validations[i].item, validations[i].rating, pred, err);
+	int ridx= 0;
+	for(int u = 0; u < nUsers; u++) {
+		for(int i = 0; i < users[u].nu; i++,ridx++){
+			double pred = predict(u, validations[ridx].item);
+			double err = (double)validations[ridx].rating/SCORENORM - pred;
+			sq += err*err*SCORENORM*SCORENORM;
+			if (print) {
+				printf("%d %d %d %g %g\n", u, validations[i].item, validations[i].rating, pred*SCORENORM, SCORENORM*err);
+			}
 		}
 	}
 	return sqrt(sq/nValidations);
 
 }
+
+void make_predictions()
+{
+	FILE *fp = fopen("predictions.txt", "w");
+	int ridx=0;
+	for(int u = 0; u < nUsers; u++) {
+		for(int r = 0; r < users[u].qu; r++) {
+			double pred = predict(u,tests[ridx].item);
+			fprintf(fp, "%lg\n", pred*SCORENORM);
+		}
+	}
+	fclose(fp);
+}
+
+void read_tests()
+{
+	int uid, n, iid, stmp, day, hr, min, sec;
+	double  score;
+	ssize_t didx = 0;
+	unsigned int uidx=0, ridx=0, iidx=0;
+	ssize_t size;
+	char *data = (char *)open_read(TESTFILE, &size);
+	int sum = 0;
+	while(1) {
+		if ( parseInt(data, &didx, &uid, size) ) {
+			break;
+		}
+		uidx = umap[uid];
+		parseInt(data, &didx, &n, size);
+		if (n != 6) printf("Error size not 6!=%d\n", n);
+		users[uidx].qu = n;
+		for(int i =0; i < n; i++) {
+			parseInt(data, &didx, &iid, size);
+			parseInt(data, &didx, &day, size);
+			parseInt(data, &didx, &hr, size);
+			parseInt(data, &didx, &min, size);
+			parseInt(data, &didx, &sec, size);
+			score = (double) stmp;
+			if( imap.count(iid) == 0) {
+				iidx = imaplen;
+				imaplen ++;
+				imap[iid] = iidx;
+
+				items[iidx].id = iid;
+				items[iidx].count = 0;
+			} else {
+				iidx = imap[iid];
+			}
+			tests[ridx].item = iidx;
+			tests[ridx].rating = stmp;
+			ridx++;
+		}
+	}
+	munmap(data, size);
+	nTests = ridx;
+	printf("nTests = %d %d\n", nTests, ridx);
+}
+
 
 void read_validate()
 {
@@ -287,8 +352,6 @@ void read_validate()
 			parseInt(data, &didx, &min, size);
 			parseInt(data, &didx, &sec, size);
 			score = (double) stmp;
-			score /= SCORENORM;
-			//printf("%f\n", score);
 			if( imap.count(iid) == 0) {
 				iidx = imaplen;
 				imaplen ++;
@@ -301,13 +364,8 @@ void read_validate()
 			} else {
 				iidx = imap[iid];
 			}
-			//users[uidx].count += 1;
-			//users[uidx].sum += score;
-			//items[iidx].count += 1;
-			//items[iidx].sum += score;
 			validations[ridx].item = iidx;
-			validations[ridx].user = uidx;
-			validations[ridx].rating = score;
+			validations[ridx].rating = stmp; //score;
 			ridx++;
 		}
 	}
@@ -324,7 +382,7 @@ void *train_model(void *ptr = NULL) {
 	}
 	double lasterr = 1e6+1, sq = 0, err = 1e6;
 	int epoch = 0, faults = 0;
-	while (faults <= 2 && epoch < maxepochs) {
+	while (faults <  maxfaults && epoch < maxepochs) {
 	//while (epoch < maxepochs) { // && (epochs < 10 || (lasterr - err) > 1e-6))
 		lasterr = err;
 		sq = 0;
@@ -338,20 +396,26 @@ void *train_model(void *ptr = NULL) {
 			if (user.count > 0) {
 				invsqru = sqrt( 1. / (double)user.count);
 			}
-			if(user.nu > 0) {
-				invsqnu = sqrt( 1. / (double)user.nu);
-			}
+			invsqnu = sqrt( 1. / (double)(user.nu +user.count+user.qu));
 			int uf =  user.id*nFeatures;
 			for(int f = 0; f < nFeatures; f++) {
 				p[uf + f] = 0;
 
 				for(int r = 0; r < user.count; r++) {
-					float rb = ratings[ridx+r].rating;
+					float rb = (double)ratings[ridx+r].rating / SCORENORM;
 					rb -= mu;
-					rb -= bu[ratings[ridx+r].user];
+					rb -= bu[uidx];
 					rb -= bi[ratings[ridx+r].item];
 					int j = (ratings[ridx+r].item*nFeatures);
 					p[uf+f] += rb * x[j+f] * invsqru;
+					p[uf+f] += y[j+f] * invsqnu;
+				}
+				for(int r = uidx*4; r < (uidx+1)*4; r++) {
+					int j = (validations[r].item*nFeatures);
+					p[uf+f] += y[j+f] * invsqnu;
+				}
+				for(int r = uidx*6; r < (uidx+1)*6; r++) {
+					int j = (tests[r].item*nFeatures);
 					p[uf+f] += y[j+f] * invsqnu;
 				}
 				sum[f] = 0.;
@@ -359,34 +423,48 @@ void *train_model(void *ptr = NULL) {
 			//pthread_barrier_wait(&barrier);
 			for(int r = 0; r < user.count; r++ ) {
 				struct rating_s rating = ratings[ridx+r];
-				double pred = mu + bi[rating.item] + bu[rating.user];
+				double pred = mu + bi[rating.item] + bu[uidx];
 				for(int f1 = 0; f1 < nFeatures; f1++) {
-					pred += p[rating.user*nFeatures + f1]*q[rating.item*nFeatures+f1];
+					pred += p[uf + f1]*q[rating.item*nFeatures+f1];
 				}
-				err = rating.rating - pred;
+				err = rating.rating / SCORENORM - pred;
 				sq += err*err*SCORENORM*SCORENORM;
 
 				for(int f = 0; f < nFeatures; f++) {
 
 					double tmpq = q[rating.item*nFeatures+f];
-					double tmpp = p[rating.user*nFeatures+f];
+					double tmpp = p[uf + f];
 
 					sum[f] += err*tmpq;
 
 					q[rating.item*nFeatures+f] += GAMMA*(err*tmpp - LAMBDA*tmpq);
-					//p[rating.user*nFeatures+f] += GAMMA*(err*tmpq - LAMBDA*tmpp);
+					//p[uidx*nFeatures+f] += GAMMA*(err*tmpq - LAMBDA*tmpp);
 				}
-				bu[rating.user] += GAMMA*(err - LAMBDA*bu[rating.user]);
+				bu[uidx] += GAMMA*(err - LAMBDA*bu[uidx]);
 				bi[rating.item] += GAMMA*(err - LAMBDA*bi[rating.item]);
 			}
 
 
 			for(int r = 0; r < user.count; r++) {
-				struct rating_s rating = ratings[ridx + r];
-				float rb = rating.rating - mu -bu[rating.user] -bi[rating.item];
+				struct rating_s rating = ratings[ridx + r] ;
+				float rb = (double)rating.rating/SCORENORM - mu -bu[uidx] -bi[rating.item];
 				for(int f = 0; f < nFeatures; f++) {
 					int i = rating.item*nFeatures + f;
 					x[i] += GAMMA2*(invsqru*rb*sum[f] - LAMBDA2*x[i]);
+					y[i] += GAMMA2*(invsqnu*sum[f] - LAMBDA2*y[i]);
+				}
+			}
+			for(int r = 4*uidx; r < 4*(uidx+1); r++) {
+				struct rating_s rating = validations[r];
+				for(int f = 0; f < nFeatures; f++) {
+					int i = rating.item*nFeatures + f;
+					y[i] += GAMMA2*(invsqnu*sum[f] - LAMBDA2*y[i]);
+				}
+			}
+			for(int r = 6*uidx; r < 6*(uidx+1); r++) {
+				struct rating_s rating = tests[r];
+				for(int f = 0; f < nFeatures; f++) {
+					int i = rating.item*nFeatures + f;
 					y[i] += GAMMA2*(invsqnu*sum[f] - LAMBDA2*y[i]);
 				}
 			}
@@ -396,7 +474,7 @@ void *train_model(void *ptr = NULL) {
 		double verr = validate();
 		err = sqrt(sq / nRatings);
 		printf("epoch=%d RMSE=%g VMRSE=%g" , epoch++, err, verr);
-		printf(" time=%g rank=%d\n", start-get_time(), rank);
+		printf(" time=%g rank=%d\n", get_time()-start, rank);
 		if (verr > lasterr) {
 			faults++;
 		} else {
@@ -463,12 +541,12 @@ void read_data()
 			umaplen++;
 			umap[uid] = uidx;
 			users[uidx].id = uid;
-			users[uidx].count = 0;
 			//users[uidx].sum = 0;
 		} else {
 			uidx = imap[uid];
 		}
 		parseInt(data, &didx, &n, size);
+		users[uidx].count = n;
 		for(int i =0; i < n; i++) {
 			parseInt(data, &didx, &iid, size);
 			parseInt(data, &didx, &stmp, size);
@@ -477,8 +555,8 @@ void read_data()
 			parseInt(data, &didx, &min, size);
 			parseInt(data, &didx, &sec, size);
 			score = (double) stmp;
+			//score /= SCORENORM;
 			mu += score;
-			score /= SCORENORM;
 			if( imap.count(iid) == 0) {
 				iidx = imaplen;
 				imaplen ++;
@@ -486,31 +564,27 @@ void read_data()
 
 				items[iidx].id = iid;
 				items[iidx].count = 0;
-				//items[iidx].sum = 0;
 			} else {
 				iidx = imap[iid];
 			}
-			users[uidx].count += 1;
-			//users[uidx].sum += score;
 			items[iidx].count += 1;
-			//items[iidx].sum += score;
 			ratings[ridx].item = iidx;
-			ratings[ridx].user = uidx;
 			ratings[ridx].rating = score;
 			ridx++;
 		}
 	}
 	munmap(data, size);
 	read_validate();
+	read_tests();
 	nItems = imaplen;
 	nUsers = umaplen;
 	nRatings = ridx;
-	mu /= (double)ridx; //  * SCORENORM;
+	mu /= (double)ridx * SCORENORM;
 	printf("nRatings = %d\n", nRatings);
 	printf("mu = %lg\n", mu);
 
 	FILE *tmp = fopen("tmp/values", "w");
-	fprintf(tmp, "%u %u %u %25.18g\n", nRatings, nUsers, nItems, mu);
+	fprintf(tmp, "%u %u %u %u %u %25.18lg\n", nRatings, nUsers, nItems, nValidations, nTests, mu);
 	for(int i = 0 ; i < nUsers; i++ ) {
 		fprintf(tmp, "%d\n", users[i].id);
 	}
@@ -518,6 +592,38 @@ void read_data()
 		fprintf(tmp, "%d\n", items[i].id);
 	}
 	fclose(tmp);
+}
+
+void read_stats()
+{
+	FILE *fp = fopen("stats1.txt", "r");
+	char buf[512], *name, *value;
+	//char *buf = tmp;
+	unsigned long int d;
+	while(fgets(buf, 512, fp) != NULL) {
+		name = strtok(buf, "=");
+		value = strtok(NULL, "=");
+		d = strtoul(value, NULL, 10);
+		if(strncmp(name, "nUsers", 512) == 0) {
+			nUsers = d;
+		} else if(strncmp(name, "nItems", 512) == 0) {
+			nItems = d;
+		} else if(strncmp(name, "nRatings", 512) == 0) {
+			//pass
+		} else if(strncmp(name, "nTrainRatings", 512) == 0) {
+			nRatings = d;
+		} else if(strncmp(name, "nProbeRatings", 512) == 0) {
+			nValidations = d;
+		} else if(strncmp(name, "nTestRatings", 512) == 0) {
+			nTests = d;
+		} else {
+			fprintf(stderr, "ERROR unknown value in stats1.txt\n");
+			exit(1);
+		}
+		fprintf(stdout, "%s=%lu\n", name, d);
+	}
+
+	fclose(fp);
 }
 
 void print_model_stats()
@@ -551,7 +657,8 @@ int main (int argc, char **argv)
 	bool isVerify = false;
 	bool isBuildModel = false;
 	bool isStats = false;
-	while ( (c = getopt(argc, argv, "itvme:s")) != -1) {
+	bool isPredict = false;
+	while ( (c = getopt(argc, argv, "itvme:spf:bE:")) != -1) {
 		switch (c) {
 			case 'i':
 				isInit = true;
@@ -571,6 +678,15 @@ int main (int argc, char **argv)
 			case 'e':
 				maxepochs = atoi(optarg);
 				break;
+			case 'E':
+				maxepochsbias = atoi(optarg);
+				break;
+			case 'f':
+				maxfaults = atoi(optarg);
+				break;
+			case 'p':
+				isPredict = true;
+				break;
 			case '?':
 				break;
 			default:
@@ -584,9 +700,11 @@ int main (int argc, char **argv)
 	}
 	pthread_barrier_init(&barrier, NULL, NUM_THREADS);
 
+	read_stats();
+
 	if(!isInit) {
 		FILE *tmp = fopen("tmp/values", "r");
-		fscanf(tmp, "%u %u %u %lg\n", &nRatings, &nUsers, &nItems, &mu);
+		fscanf(tmp, "%u %u %u %u %u %lg\n", &nRatings, &nUsers, &nItems, &nValidations, &nTests, &mu);
 		int lk = 0;
 		for(int i = 0 ; i < nUsers; i++ ) {
 			fscanf(tmp, "%d\n", &lk);
@@ -599,15 +717,11 @@ int main (int argc, char **argv)
 		fclose(tmp);
 	}
 
-	printf("<");
 	load_model();
-	printf(">\n");
 
-	printf("<"); fflush(stdout);
 	if(isInit) {
 		read_data();
 	}
-	printf(">\n");
 
 	if (isInit || isBuildModel) {
 		kickoff(init_model);
@@ -625,6 +739,10 @@ int main (int argc, char **argv)
 
 	if (isVerify) {
 		printf("VRMSE = %g\n", validate(true));
+	}
+
+	if(isPredict) {
+		read_tests();
 	}
 	pthread_barrier_destroy(&barrier);
 	pthread_exit(NULL);
